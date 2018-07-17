2b92a12ce35e1564ed11da506392378a77afe553
Boris <oreqizer@users.noreply.github.com>
2018-07-17 13:20:16 +0200

    Add ESLint, Flow, editorconfig, prettier, Jest, storybook (#10)

---
+++ stories/Text.stories.js
@@ -0,0 +1,8 @@
+// @flow strict
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { withKnobs, text } from "@storybook/addon-knobs/react";
+
+storiesOf("Text", module)
+  .addDecorator(withKnobs)
+  .add("default", () => <span>{text("Text", "kek")}</span>);
