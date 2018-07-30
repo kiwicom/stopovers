40744784bf6c47ccaabcd45a0c5bf87f5ef9af88
Alex Alexeev <work2alex@gmail.com>
2018-07-30 10:51:37 +0200

    Fix ActionText ssr

--- components/shared/ActionText.js
+++ components/shared/ActionText.js
@@ -1,8 +1,12 @@
 // @flow
 
-import Text from "./Text";
+import styled from "styled-components";
 
-const ActionText = Text.extend`
+const ActionText = styled.div`
+  color: #46515e;
+  @media (min-width: 1440px) {
+    font-size: 20px;
+  }
   font-weight: 300;
   line-height: 1.7;
   margin: 0 16px;
