e496d4d54dc25492fba162e45fbb15a925c25fd1
Yuri Yakovlev <hi@mynameisyuri.com>
2018-08-01 15:22:43 +0200

    Translate Hero section

--- components/hero/SearchAction.js
+++ components/hero/SearchAction.js
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import { AirplaneRight } from "@kiwicom/orbit-components/lib/icons";
+import Text from "@kiwicom/nitro/lib/components/Text";
 
 import { scrollToElement } from "../helpers";
 import Button from "../shared/Button";
@@ -10,14 +11,16 @@ import ActionText from "../shared/ActionText";
 const SearchAction = () => (
   <>
     <AirplaneRight customColor="#bac7d5" />
-    <ActionText>Search for flights with a one-day stopover in Dubai</ActionText>
+    <ActionText>
+      <Text t="searchWithStopover" />
+    </ActionText>
     <Button
       fontSize={14}
       onClick={() => {
         scrollToElement("search");
       }}
     >
-      Go to search
+      <Text t="goToSearch" />
     </Button>
   </>
 );
