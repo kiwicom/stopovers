5dc160bacbbb8e6efb8970e70519d62f9df5392a
Alex Alexeev <work2alex@gmail.com>
2018-08-08 16:23:58 +0200

    Add pulse to hero button

--- components/hero/Action.js
+++ components/hero/Action.js
@@ -13,16 +13,30 @@ const ActionButton = styled.button`
   height: 52px;
   border-radius: 50%;
   padding: 0;
-  box-shadow: 0px 0px 0px 6px rgba(178, 229, 222, 1);
+  box-shadow: 0 0 0 0 rgba(178, 229, 222, 0);
   border: 0;
   cursor: pointer;
   background: #00a991;
   order: 1;
   justify-self: end;
+  animation: pulse 2s infinite;
 
   @media (min-width: 740px) {
     margin-right: 20px;
     order: 0;
+  }
+  @keyframes pulse {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.05);
+      box-shadow: 0 0 0 12px rgba(178, 229, 222, 0.8);
+    }
+    100% {
+      transform: scale(1);
+      box-shadow: 0 0 0 0 rgba(178, 229, 222, 0);
+    }
   }
 `;
 
