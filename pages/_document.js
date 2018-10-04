813cbe6df374fc2868be40e94610189bf45821ce
Yuri Yakovlev <hi@mynameisyuri.com>
2018-10-04 12:09:26 +0200

    Bump deps

--- pages/_document.js
+++ pages/_document.js
@@ -150,7 +150,6 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-          <title>Stopovers</title>
           {this.props.styleTags}
           <link
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
