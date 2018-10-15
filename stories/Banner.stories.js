b019952f8713149d0c45939941b07173aa0b2b96
Angelo Gazzola <angelo.gazzola@kiwi.com>
2018-10-15 10:20:36 +0200

    Convert Banner to flexbox

---
+++ stories/Banner.stories.js
@@ -0,0 +1,8 @@
+// @flow
+
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Banner from "../components/banner/Banner";
+
+storiesOf("Banner", module).add("default", () => <Banner />);
