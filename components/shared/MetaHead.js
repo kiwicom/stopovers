be32bce99d8ce156c7656d8b6ca7ee9afdf7dda5
Alex Alexeev <alex.alexeev@kiwi.com>
2018-10-22 13:49:03 +0200

    Translate meta tags

--- components/shared/MetaHead.js
+++ components/shared/MetaHead.js
@@ -3,48 +3,61 @@
 import * as React from "react";
 import Head from "next/head";
 import translate from "@kiwicom/nitro/lib/services/intl/translate";
+import omit from "lodash/omit";
+
+type Tags = {
+  [key: string]: { value: string },
+};
 
 type Props = {
-  translations: { [key: string]: string },
+  translations?: {
+    metaDescription?: string,
+    metaTitle?: string,
+    otherMetaTags?: Tags,
+  },
+  locale: string,
+  currentPath: string,
+  socialPhotos: {
+    twitter: ?string,
+    facebook: ?string,
+  },
 };
 
 // TODO: make meta tags dynamic via DatoCMS
-const MetaHead = ({ translations }: Props) => {
-  const title = translate(translations, "metaTitle");
-  const description = translate(translations, "metaDescription");
+const MetaHead = ({ translations, locale, currentPath, socialPhotos }: Props) => {
+  const currentUrl = `https://www.kiwi.com${currentPath.split(/[?#]/)[0]}/`;
+  const stringTranslations = translations ? omit(translations, "otherMetaTags") : {};
+  const title = translate(stringTranslations, "metaTitle");
+  const description = translate(stringTranslations, "metaDescription");
+  const otherMetaTags =
+    translations && translations.otherMetaTags ? translations.otherMetaTags : {};
   return (
     <Head>
       <title>{title}</title>
+      <meta charSet="utf-8" />
       <meta name="description" content={description} />
-      <link rel="canonical" href="https://www.kiwi.com/en/stopovers/dubai/" />
+      <link rel="canonical" href={currentUrl} />
 
-      <meta property="og:locale" content="en_US" />
+      <meta property="og:locale" content={locale} />
       <meta property="og:type" content="article" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta
-        property="og:image"
-        content="https://www.datocms-assets.com/7631/1539703866-dubaifacebook1200x630.png"
-      />
+      <meta property="og:image" content={socialPhotos.facebook || ""} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
 
-      <meta property="og:url" content="https://www.kiwi.com/stopovers/dubai/" />
+      <meta property="og:url" content={currentUrl} />
       <meta property="og:site_name" content="Kiwi.com" />
 
-      <meta property="article:tag" content="Dubai stopovers" />
-      <meta property="article:tag" content="Dubai" />
-      <meta property="article:tag" content="Flights from Europe to Asia" />
-      <meta property="article:tag" content="Flights from Asia to Europe" />
+      {Object.keys(otherMetaTags).map(key => (
+        <meta property="article:tag" key={key} content={otherMetaTags[key].value || ""} />
+      ))}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@kiwicom247" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content="https://www.datocms-assets.com/7631/1539703853-dubaitwittersummaryimage1200x643.jpg"
-      />
+      <meta name="twitter:image" content={socialPhotos.twitter || ""} />
     </Head>
   );
 };
