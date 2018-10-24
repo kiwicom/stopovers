093017dcf3edf5df5ff6f837f93e3a4a628ed3e8
Angelo Gazzola <angelo@nglgzz.com>
2018-10-24 17:44:19 +0200

    Add pushTranslations script (#242)

---
+++ scripts/pushTranslations.js
@@ -0,0 +1,44 @@
+// @flow
+
+import fetch from "isomorphic-unfetch";
+import dotenv from "dotenv";
+import fs from "fs";
+import FormData from "form-data";
+
+dotenv.config();
+const env = name => process.env[name] || "";
+
+const PHRASE_APP_BASE_URL = "https://api.phraseapp.com/api/v2";
+const URL = `${PHRASE_APP_BASE_URL}/projects/${env("PHRASE_APP_PROJECT_ID")}/uploads`;
+
+const filenames = ["static/locales/en-GB.json"];
+const options = {
+  method: "POST",
+  headers: {
+    // `Content-Type` must not be specified when using FormData.
+    Authorization: `token ${env("PHRASE_APP_ACCESS_TOKEN")}`,
+  },
+};
+
+(async () => {
+  try {
+    const results = {};
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const filename of filenames) {
+      const file = fs.createReadStream(filename);
+      const body = new FormData();
+      body.append("file_format", "json");
+      body.append("file", file);
+      body.append("locale_id", env("PHRASE_APP_LOCALE_ID"));
+
+      const response = await fetch(URL, { ...options, body });
+      results[filename] = (await response.json()).summary;
+    }
+
+    console.table(results); // eslint-disable-line no-console
+  } catch (error) {
+    console.error(error); // eslint-disable-line no-console
+    process.exit(1);
+  }
+})();
