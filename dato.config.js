16e16bac82eb0ff7401b32b1e4465df6010e074f
Alex Alexeev <work2alex@gmail.com>
2018-11-26 12:15:20 +0100

    Translate image alts and titles (#294)
    * Translate image alts and titles
    
    * Refactor dato config
    
    * Process images on itineraries

--- dato.config.js
+++ dato.config.js
@@ -1,28 +1,71 @@
 // @flow
 
-const filterByKeys = (data, filterKeys, areIncluded, noEmptyValues) =>
-  Object.keys(data)
-    .filter(key => (areIncluded ? filterKeys.includes(key) : !filterKeys.includes(key)))
-    .reduce((obj, key) => {
-      if (noEmptyValues && !data[key]) {
-        return obj;
-      }
-      return {
-        ...obj,
-        [key]: data[key],
-      };
-    }, {});
+const cleanEmptyValues = data =>
+  typeof data !== "object"
+    ? data
+    : Object.keys(data).reduce(
+        (obj, key) => ({
+          ...obj,
+          ...(data[key] ? { [key]: cleanEmptyValues(data[key]) } : null),
+        }),
+        {},
+      );
 
-const formatList = (list, translatedFields, isTranslatedPart, callback) =>
-  list.reduce((result, item) => {
-    const id = item.id || callback(item);
+const filterByKeys = (data, filterKeys, areIncluded) =>
+  Object.keys(data)
+    .filter(key => areIncluded === filterKeys.includes(key))
+    .reduce((obj, key) => ({ ...obj, [key]: data[key] }), {});
+
+const listToObj = (list, filterKeys, areIncluded, getID) =>
+  list.reduce((obj, item) => {
+    const id = item.id || getID(item);
     return {
-      ...result,
-      [id]: filterByKeys(item, translatedFields, isTranslatedPart, isTranslatedPart),
+      ...obj,
+      [id]: filterByKeys(item, filterKeys, areIncluded),
     };
   }, {});
 
-const getSlideId = slide => slide.url.match(/(\d+)[^/]+(?=\.\w+$)/)[1];
+const formatImage = (data, isTranslatedPart, getPhoto) => {
+  const photo = getPhoto ? getPhoto(data) : data.photo;
+  if (!photo) return data;
+
+  // For articles, titles and alt text of pictures don't need to be translated.
+  if (data.itemType === "article") {
+    return isTranslatedPart ? null : photo;
+  }
+
+  return filterByKeys(photo, ["alt", "title"], isTranslatedPart);
+};
+
+const formatImages = (images, isTranslatedPart, getPhoto) => {
+  const getImageId = img => img.url.match(/(\d+)[^/]+(?=\.\w+$)/)[1];
+  return images.reduce((obj, image) => {
+    const formattedImage = cleanEmptyValues(formatImage(image, isTranslatedPart, getPhoto));
+    return {
+      ...obj,
+      [getImageId(image)]: formattedImage,
+    };
+  }, {});
+};
+
+const formatItineraries = (itineraries, isTranslatedPart) =>
+  itineraries.reduce((result, itinerary) => {
+    const tips = cleanEmptyValues(
+      listToObj(itinerary.tips, ["title", "description"], isTranslatedPart),
+    );
+
+    itinerary.tips.forEach(tip => {
+      tips[tip.id].photo = formatImage(tip, isTranslatedPart);
+    });
+
+    return {
+      ...result,
+      [itinerary.id]: {
+        ...filterByKeys(itinerary, ["title"], isTranslatedPart),
+        tips,
+      },
+    };
+  }, {});
 
 const nonTranslatedKeys = [
   "id",
@@ -51,31 +94,10 @@ module.exports = (dato, root) => {
   const formattedData = dato.cities.reduce(
     (aggregated, city) => {
       const allData = city.toMap();
-      const translatedData = filterByKeys(allData, nonTranslatedKeys, false, true);
+      const translatedData = filterByKeys(allData, nonTranslatedKeys, false);
       const nonTranslatedData = filterByKeys(allData, nonTranslatedKeys, true);
       const cityTag = `${city.name.replace(/ /g, "-").toLowerCase()}_${city.id}`;
-
       const translatedTagFields = ["value"];
-      const translatedItineraryFields = ["title"];
-      const translatedTipFields = ["title", "description"];
-      const translatedSlideFields = ["alt", "title"];
-
-      const formatItineraries = (rawItineraries, isTranslatedPart) =>
-        rawItineraries.reduce(
-          (result, itinerary) => ({
-            ...result,
-            [itinerary.id]: {
-              ...filterByKeys(
-                itinerary,
-                translatedItineraryFields,
-                isTranslatedPart,
-                isTranslatedPart,
-              ),
-              tips: formatList(itinerary.tips, translatedTipFields, isTranslatedPart),
-            },
-          }),
-          {},
-        );
 
       return {
         translated: {
@@ -83,34 +105,28 @@ module.exports = (dato, root) => {
           [cityTag]: {
             ...translatedData,
             itineraries: formatItineraries(nonTranslatedData.itineraries, true),
-            otherMetaTags: formatList(nonTranslatedData.otherMetaTags, translatedTagFields, true),
-            sliderPhotos: formatList(
-              nonTranslatedData.sliderPhotos,
-              translatedSlideFields,
-              true,
-              getSlideId,
-            ),
+            otherMetaTags: listToObj(nonTranslatedData.otherMetaTags, translatedTagFields, true),
+            sliderPhotos: formatImages(nonTranslatedData.sliderPhotos, true, photo => photo),
+            mainPhoto: formatImage(nonTranslatedData, true, data => data.mainPhoto),
           },
         },
         nonTranslated: {
           ...aggregated.nonTranslated,
           [cityTag]: {
             ...nonTranslatedData,
-            articles: formatList(nonTranslatedData.articles, []),
-            itineraries: formatItineraries(nonTranslatedData.itineraries),
-            otherMetaTags: formatList(nonTranslatedData.otherMetaTags, translatedTagFields),
-            sliderPhotos: formatList(
-              nonTranslatedData.sliderPhotos,
-              translatedSlideFields,
-              false,
-              getSlideId,
-            ),
+            articles: listToObj(nonTranslatedData.articles, [], false),
+            itineraries: formatItineraries(nonTranslatedData.itineraries, false),
+            otherMetaTags: listToObj(nonTranslatedData.otherMetaTags, translatedTagFields, false),
+            sliderPhotos: formatImages(nonTranslatedData.sliderPhotos, false, photo => photo),
+            mainPhoto: formatImage(nonTranslatedData, false, data => data.mainPhoto),
           },
         },
       };
     },
     { translated: {}, nonTranslated: {} },
   );
+
+  formattedData.translated = cleanEmptyValues(formattedData.translated);
 
   // en-GB translation for all cities as a source of truth
   // sent to phraseApp on each build (pushTranslations script)
